c04a79d60d5971e888f6032ab4c1a3d455a660d1
dineshsel <dinesh.paramasivam@hotmail.com>
2025-05-24 22:22:08 +0530

    Add files via upload

---
+++ day1/Chrome.java
@@ -0,0 +1,19 @@
+package work.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version=91.0f;
+		String developer="Google";
+		boolean isBeta=true;
+		int releaseType = 200;
+		char shortcutKey='C';
+		System.out.println(version);
+		System.out.println(developer);
+		System.out.println(isBeta);
+		System.out.println(releaseType);
+		System.out.println(shortcutKey);
+	}
+
+}
